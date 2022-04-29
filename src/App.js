249db76ceeb13633bb623de05610dac3223ebdfa
Houcine <houcine.boukhari@outlook.com>
2022-04-29 17:23:55 +0100

    image input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import './App.css';
-import Home from './views/Home';
+import "./App.css";
+import Home from "./views/Home";
 function App() {
-  return (
-      <Home />
-  );
+  return <Home />;
 }
 
 export default App;
