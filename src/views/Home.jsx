8c39a0897057e45c8fd4dceed51eab5bd7ced17d
Houcine <houcine.boukhari@outlook.com>
2022-05-01 13:51:46 +0100

    receive image

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 function Home() {
   const [file, setFile] = useState("");
   const [preview, setPreview] = useState(null);
+  const [result, setResult] = useState(null);
   const handleChange = (e) => {
     const current = e.target.files[0];
     setFile(current);
@@ -15,12 +16,34 @@ function Home() {
       reader.onloadend = () => setPreview(reader.result);
     }
   }, [file]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", file);
+    axios
+      .post("/process", formData)
+      .then((res) => {
+        const reader = new FileReader();
+        console.log(res);
+        reader.readAsDataURL(new Blob([res.data], { type: "image/png" }));
+        reader.onloadend = () => setResult(reader.result);
+      })
+      .catch((err) => console.warn(err));
+  };
+  console.log(result);
   return (
     <div className="h-screen flex justify-center items-center">
-      <form>
+      <form className="flex flex-col items-center" onSubmit={handleSubmit}>
         <input type="file" accept="image/*" onChange={handleChange} />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-10 py-1 mt-2 rounded"
+        >
+          Submit
+        </button>
       </form>
-      <img src={preview} alt="" />
+      <img src={preview} alt="" className="h-32 w-32" />
+      <img src={result} alt="" className="h-32 w-32" />
     </div>
   );
 }
